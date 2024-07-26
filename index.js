58767f3ed5db63c8ecfb56ff5c82056abec8e4fa
philipp <philipp@steckenbauer.com>
2024-07-26 15:28:23 +0700

    Added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Meta = require('./src/Meta/Meta');
+const Option = require('./src/Option/Option');
+const Result = require('./src/Result/Result');
+const { panic } = require('./src/panic/panic');
+
+module.exports = {
+	Option,
+	Result,
+	Meta,
+	panic
+};
